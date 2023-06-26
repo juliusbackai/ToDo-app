adf6fc470acc9eaa933efe41bb4e6fddd4fcfe55
Julo Backai <julobackai@gmail.com>
2023-06-26 12:31:00 +0200

    aws calculator

--- spring-boot-server/src/test/java/com/bezkoder/spring/jpa/postgresql/SpringBootJpaPostgresqlApplicationTests.java
+++ spring-boot-server/src/test/java/com/bezkoder/spring/jpa/postgresql/SpringBootJpaPostgresqlApplicationTests.java
@@ -1,13 +1,23 @@
 package com.bezkoder.spring.jpa.postgresql;
 
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
 class SpringBootJpaPostgresqlApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void testSum() {
+		Calculator calculator = new Calculator();
+		assertEquals(2, calculator.sum(1,1));
+		assertNotEquals(3, calculator.sum(1,1));
 	}
 
 }
